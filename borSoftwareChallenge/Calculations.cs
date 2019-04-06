523f4c5a7fc9ae41b02de9f11b6f9ec19b6acf0a
Will Scargill <willscargill@outlook.com>
2019-04-06 16:39:00 +0100

    Added comments

--- borSoftwareChallenge/Calculations.cs
+++ borSoftwareChallenge/Calculations.cs
@@ -11,38 +11,35 @@ namespace borSoftwareChallenge
 
         public static void FillOutputs()
         {
+            // Setting up values as doubles
             double roomWidth = Convert.ToDouble(MainWindow.main.tbRoomWidth.Text);
             double roomLength = Convert.ToDouble(MainWindow.main.tbRoomLength.Text);
             double roomHeight = Convert.ToDouble(MainWindow.main.tbRoomHeight.Text);
 
             double floorArea = roomLength * roomWidth;
 
-            double wallArea = (roomWidth * roomHeight) * 2 + (roomLength * roomHeight) * 2; 
+            double wallArea = (roomWidth * roomHeight) * 2 + (roomLength * roomHeight) * 2;
 
             double roomVol = roomWidth * roomHeight * roomLength; 
           
-            if (MainWindow.main.recesses.Count == 0) { }
+            if (MainWindow.main.recesses.Count == 0) { } // If there are no recesses
             else
             {
-                double additionalArea = 0;
-                double additionalVol = 0;
-
-                foreach (Recess r in MainWindow.main.recesses)
+                foreach (Recess r in MainWindow.main.recesses) // For each recess
                 {
-                    if (r.IsDoor == true)
+                    if (r.IsDoor == true) // If recess is a door
                     {
-                        floorArea += r.Width * r.Depth;
+                        floorArea += r.Width * r.Depth; // Add the floor area of the recess to overall floor area
                     }
-                    wallArea -= (r.Width * r.Height);
-                    additionalArea += r.GetArea();
-                    additionalVol += r.GetVolume();
+                    wallArea -= (r.Width * r.Height); // Remove wall area taken up by the recess
+                    wallArea += r.GetArea(); // Add recess area to overall area
+                    roomVol += r.GetVolume(); // Add recess volume to overall volume
                 }
-                wallArea += additionalArea;
-                roomVol += additionalVol;
             }
 
-            double volOfPaint = (wallArea / 32) * 2.5;
+            double volOfPaint = (wallArea / 32) * 2.5; // Calculate volume of paint required in litres based on typical coverage (32m^2 for l.5L)
 
+            // Set output textboxes values to calculated values
             MainWindow.main.tbFloorArea.Text = floorArea.ToString();
             MainWindow.main.tbPaintRequired.Text = wallArea.ToString();
             MainWindow.main.tbRoomVolume.Text = roomVol.ToString();
